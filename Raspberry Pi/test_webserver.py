431ca7f0e30bd236f1a736c263a3e04e242986ff
Wouter Bergmann Tiest <berwm@145-137-83-241.wlan.hro.nl>
2020-01-17 10:43:55 +0100

    maakt foto met *elk* request

--- Raspberry Pi/test_webserver.py
+++ Raspberry Pi/test_webserver.py
@@ -1,9 +1,17 @@
 import http.server
-import socketserver
+import os
 
-PORT = 80
-HANDLER = http.server.SimpleHTTPRequestHandler
+PORT = 8080
+DIRECTORY = "images"
 
-with socketserver.TCPServer(("", PORT), HANDLER) as httpd:
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+    def translate_path(self, path):
+        os.system("raspistill -n -t 1 -o images/image.jpg")
+        return "images/image.jpg"
+
+with http.server.HTTPServer(("", PORT), MyHandler) as httpd:
     print("serving at port", PORT)
     httpd.serve_forever()
