2bd695791a745c0ec9dc84d2c7192722e5a3cc61
Wouter Bergmann Tiest <berwm@145.137.EARLY-REGISTRATION.of.SURFnet.invalid>
2020-01-20 14:46:04 +0100

    alles geïntegreerd

--- Raspberry Pi/smartfarm.py
+++ Raspberry Pi/smartfarm.py
@@ -7,8 +7,6 @@ import Adafruit_MCP3008
 import Adafruit_DHT as DHT
 from time import sleep
 import requests
-from actuators import Actuator
-from sensor import SoilMoistureSensor, LightSensor, TemperatureSensor, HumiditySensor
 
 WINDOW_WIDTH = 480
 WINDOW_HEIGHT = 320
@@ -27,6 +25,83 @@ V3_PIN = 26
 V4_PIN = 20
 LIGHT_PIN = 21
 SOIL_PIN = 6
+
+class Sensor:
+    def __init__(self, name, unit):
+        self.name = name
+        self.unit = unit
+        self.rawValue = 0.0
+        self.value = 0.0
+
+    def calibrationFunction(self, x):
+        return x
+    
+    def getReadout(self):
+        return str(round(self.value)) + " " + self.unit
+
+class AnalogSensor(Sensor):
+    def __init__(self, name, unit, mcp, channel):
+        super().__init__(name, unit)
+        self.mcp = mcp
+        self.channel = channel
+
+    def read(self):
+        self.rawValue = self.mcp.read_adc(self.channel)
+        self.value = self.calibrationFunction(self.rawValue)
+        return self.value
+
+class DigitalSensor(Sensor):
+    def __init__(self, name, unit):
+        super().__init__(name, unit)
+
+class LightSensor(AnalogSensor):
+    def __init__(self, name, unit, mcp, channel):
+        super().__init__(name, unit, mcp, channel)
+
+class SoilMoistureSensor(AnalogSensor):
+    def __init__(self, name, unit, mcp, channel):
+        super().__init__(name, unit, mcp, channel)
+
+    def calibrationFunction(self, x):
+        return x / 10.23
+        
+class TemperatureSensor(DigitalSensor):
+    def __init__(self, name, unit):
+        super().__init__(name, unit)
+        self.sensor = DHT.DHT22
+	
+    def read(self):
+        dummy, self.rawValue = DHT.read_retry(self.sensor, DHT_PIN)
+        self.value = self.rawValue
+        return self.value
+
+class HumiditySensor(DigitalSensor):
+    def __init__(self, name, unit):
+        super().__init__(name, unit)
+        self.sensor = DHT.DHT22
+
+    def read(self):
+        self.rawValue, dummy = DHT.read_retry(self.sensor, DHT_PIN)
+        self.value = self.rawValue
+        return self.value
+
+class Actuator:
+    def __init__(self, name, gpio):
+        self.name = name
+        self.gpio = gpio
+        self.value = gp.LOW
+        gp.setmode(gp.BCM)
+        gp.setup(gpio, gp.OUT)
+
+    def set(self, value):
+        self.value = value
+        gp.output(self.gpio, value)
+
+    def on(self):
+        self.set(gp.HIGH)
+    
+    def off(self):
+        self.set(gp.LOW)
 
 class Main:
     def __init__(self):
