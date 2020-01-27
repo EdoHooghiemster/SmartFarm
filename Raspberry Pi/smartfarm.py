5cc01e53740510405a09c4601fddc7cf65d9d65f
Wouter Bergmann Tiest <berwm@145.137.EARLY-REGISTRATION.of.SURFnet.invalid>
2020-01-27 14:49:49 +0100

    soliHumidity als int ipv float

--- Raspberry Pi/smartfarm.py
+++ Raspberry Pi/smartfarm.py
@@ -535,11 +535,11 @@ class Interface:
         glColor(1, 1, 0) # yellow
         if box.big:
             self.drawText(box.x * CELL_WIDTH + 20, box.y * CELL_HEIGHT + 265, box.name)
-            self.drawSoilHumidity(box.x * CELL_WIDTH + 65, box.y * CELL_HEIGHT + 150, box.soilHumidity, box.desiredHumidity)
+            self.drawSoilHumidity(box.x * CELL_WIDTH + 65, box.y * CELL_HEIGHT + 150, int(box.soilHumidity), int(box.desiredHumidity))
             self.drawPlantGrowth(box.x * CELL_WIDTH + 50, box.y * CELL_HEIGHT + 50, int(box.plantGrowth))
         else:
             self.drawText(box.x * CELL_WIDTH + 20, box.y * CELL_HEIGHT + 120, box.name)
-            self.drawSoilHumidity(box.x * CELL_WIDTH + 20, box.y * CELL_HEIGHT + 20, box.soilHumidity, box.desiredHumidity)
+            self.drawSoilHumidity(box.x * CELL_WIDTH + 20, box.y * CELL_HEIGHT + 20, int(box.soilHumidity, box.desiredHumidity))
             self.drawPlantGrowth(box.x * CELL_WIDTH + 80, box.y * CELL_HEIGHT + 30, int(box.plantGrowth))
 
     def drawBoxes(self):
